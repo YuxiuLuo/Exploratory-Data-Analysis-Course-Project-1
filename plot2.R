d9864143b43ff31f3651a731123298a0f744b83b
YuxiuLuo <luoyuxiu@hotmail.com>
2015-04-12 22:04:02 +1000

    course project 1

---
+++ plot2.R
@@ -0,0 +1,28 @@
+#Load and prepare the household power consumption dataset for producing the corresponding plot
+data <- read.table("household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+dim(data) # 2075259 9
+attach(data)
+## We only need data of 2 days.
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) # 2880   10
+attach(newData)
+
+
+## R scripts for the plot 2
+png(filename = "plot2.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
